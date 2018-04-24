402700af41a690fca97fd394957917b91bde5d34
ILYA UTS <ilya.uts@jpmorgan.com>
2018-04-24 09:51:40 -0400

    Removed old auth code

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ var {
 } = require('googleapis');
 const OAuth2Client = google.auth.OAuth2;
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-const TOKEN_PATH = 'credentials.json';
 
 const path = require('path');
 
@@ -29,60 +28,6 @@ server.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "*");
   next();
 })
-
-
-/**
- * Create an OAuth2 client with the given credentials
- * @param {Object} credentials The authorization client credentials.
- */
-function authorize(credentials) {
-  const {
-    client_secret,
-    client_id,
-    redirect_uris
-  } = credentials.installed;
-  const oAuth2Client = new OAuth2Client(client_id, client_secret, redirect_uris[0]);
-
-  // Check if we have previously stored a token.
-  return new Promise((res, rej) => {
-    fs.readFile(TOKEN_PATH, (err, token) => {
-      if (err) rej(getNewToken(oAuth2Client));
-      oAuth2Client.setCredentials(JSON.parse(token));
-      res(oAuth2Client);
-    });
-  });
-}
-
-/**
- * Get and store new token after prompting for user authorization
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- */
-function getNewToken(oAuth2Client) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'online',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  return new Promise((good, bad) => {
-    rl.question('Enter the code from that page here: ', (code) => {
-      rl.close();
-      oAuth2Client.getToken(code, (err, token) => {
-        if (err) return bad(err);
-        oAuth2Client.setCredentials(token);
-        // Store the token to disk for later program executions
-        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-          if (err) console.error(err);
-          console.log('Token stored to', TOKEN_PATH);
-        });
-        return good(oAuth2Client);
-      });
-    });
-  });
-}
 
 /**
  * Prints the names and majors of students in a sample spreadsheet:
