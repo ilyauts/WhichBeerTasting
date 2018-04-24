30e844eb12d5d81a3601e71b97e7004cafc7aae4
ILYA UTS <ilya.uts@jpmorgan.com>
2018-04-24 01:31:08 -0400

    Added app

---
+++ public/index.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+
+    let data = {
+        companies: [],
+        beers: []
+    };
+
+    // Load beers
+    axios({
+        url: '/filter',
+        method: 'get',
+    }).then(response => {
+        // Clear old list
+        $('#beer-companies').empty();
+        $('#beer-names').empty();
+
+        data = response.data;
+
+        // Push beers
+        for (let beer of data.beers) {
+            $('#beer-names').append('<p data-beer="' + beer + '">' + beer + '</p>');
+        }
+
+        // Push companies
+        for (let company of data.companies) {
+            $('#beer-companies').append('<p data-company="' + company + '">' + company + '</p>');
+        }
+    });
+
+
+    $("#query").on("change paste keyup", function () {
+        if (this && $(this)) {
+            frontEndFilter($(this).val());
+        }
+    });
+
+    function frontEndFilter(filter) {
+        // Loop through all the companies and the beers
+        let beers = $('#beer-names p');
+        let companies = $('#beer-companies p');
+
+        for(let beer of beers) {
+            let currBeerName = $(beer).data('beer').toString();
+            if(currBeerName && currBeerName.toLowerCase().includes(filter.toLowerCase())) {
+                $(beer).show();
+            } else {
+                $(beer).hide();
+            }
+        }
+        for(let company of companies) {
+            let currCompName = $(company).data('company').toString();
+            if(currCompName && currCompName.toLowerCase().includes(filter.toLowerCase())) {
+                $(company).show();
+            } else {
+                $(company).hide();
+            }
+        }
+    }
+
+});
--- server/server.js
+++ server/server.js
@@ -1,40 +1,162 @@
-// Uncomment following to enable zipkin tracing, tailor to fit your network configuration:
-// var appzip = require('appmetrics-zipkin')({
-//     host: 'localhost',
-//     port: 9411,
-//     serviceName:'frontend'
-// });
+const express = require('express'),
+    server = express(),
+    bodyParser = require('body-parser');
+const fs = require('fs');
+const readline = require('readline');
+var {
+    google
+} = require('googleapis');
+const OAuth2Client = google.auth.OAuth2;
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const TOKEN_PATH = 'credentials.json';
 
-require('appmetrics-dash').attach();
-require('appmetrics-prometheus').attach();
-const appName = require('./../package').name;
-const express = require('express');
-const log4js = require('log4js');
-const localConfig = require('./config/local.json');
-const path = require('path');
+server.use(bodyParser.json()); // to support JSON-encoded bodies
+server.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
 
-const logger = log4js.getLogger(appName);
-const app = express();
-app.use(log4js.connectLogger(logger, { level: process.env.LOG_LEVEL || 'info' }));
-const serviceManager = require('./services/service-manager');
-require('./services/index')(app);
-require('./routers/index')(app);
+// Serve all from public
+server.use(express.static('public'));
 
-// Add your code here
+/**
+ * Create an OAuth2 client with the given credentials
+ * @param {Object} credentials The authorization client credentials.
+ */
+function authorize(credentials) {
+    const {
+        client_secret,
+        client_id,
+        redirect_uris
+    } = credentials.installed;
+    const oAuth2Client = new OAuth2Client(client_id, client_secret, redirect_uris[0]);
 
-const port = process.env.PORT || localConfig.port;
-app.listen(port, function(){
-  logger.info(`WhichBeerTasting listening on http://localhost:${port}/appmetrics-dash`);
-  
-  logger.info(`WhichBeerTasting listening on http://localhost:${port}`);
-  
-  
+    // Check if we have previously stored a token.
+    return new Promise((res, rej) => {
+        fs.readFile(TOKEN_PATH, (err, token) => {
+            if (err) rej(getNewToken(oAuth2Client));
+            oAuth2Client.setCredentials(JSON.parse(token));
+            res(oAuth2Client);
+        });
+    });
+}
+
+/**
+ * Get and store new token after prompting for user authorization
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ */
+function getNewToken(oAuth2Client) {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+    });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    return new Promise((good, bad) => {
+        rl.question('Enter the code from that page here: ', (code) => {
+            rl.close();
+            oAuth2Client.getToken(code, (err, token) => {
+                if (err) return bad(err);
+                oAuth2Client.setCredentials(token);
+                // Store the token to disk for later program executions
+                fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                    if (err) console.error(err);
+                    console.log('Token stored to', TOKEN_PATH);
+                });
+                return good(oAuth2Client);
+            });
+        });
+    });
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @param {OAuth2Client} auth The authenticated Google OAuth client.
+ */
+function listMajors(auth) {
+    const sheets = google.sheets({
+        version: 'v4',
+        auth
+    });
+    return new Promise((good, bad) => {
+        sheets.spreadsheets.values.get({
+            spreadsheetId: '1oj1MBIlzoE5AhITjd0IDKyJQVr1uwN7IC0-9va5bNTs',
+            range: 'Beer List - By Tasting!A1:CO',
+        }, (err, {
+            data
+        }) => {
+            if (err) return console.log('The API returned an error: ' + err);
+            const rows = data.values;
+            return good(rows);
+        });
+    }).catch(err => {
+        console.log('(listMajors) Error', err);
+    });
+}
+
+server.get('/', (req, res) => {
+    res.send({'msg' : `I'm Alive`})
 });
 
-app.use(function (req, res, next) {
-  res.sendFile(path.join(__dirname, '../public', '404.html'));
-})
+server.get('/filter', (req, res) => {
+    fs.readFile('client_secret.json', (err, content) => {
+        if (err) return console.log('Error loading client secret file:', err);
+        // Authorize a client with credentials, then call the Google Sheets API.
+        authorize(JSON.parse(content)).then((auth) => {
+            return listMajors(auth);
+        }).then(rows => {
+            let headers = rows[0];
 
-app.use(function (err, req, res, next) {
-  res.sendFile(path.join(__dirname, '../public', '500.html'));
-})
+            // Get a list of beer names
+            let beerNames = [],
+                companies = [];
+
+            for (let row of rows) {
+                if (row[2]) {
+                    beerNames.push(row[2]);
+                    companies.push(row[0]);
+                }
+            }
+
+            // Let's create a set
+            let companiesSet = new Set(companies);
+            let companiesShortArray = Array.from(companiesSet);
+
+            let name = req.query.name;
+            if (!name) {
+                res.send({
+                    beers: beerNames,
+                    companies: companiesShortArray
+                });
+                return;
+            }
+
+            let chillFilteredBeers = [],
+                chillFilteredCompanies = [];
+            for (let beer of beerNames) {
+                if (beer.toLowerCase().includes(name.toLowerCase())) {
+                    chillFilteredBeers.push(beer);
+                }
+            }
+            for (let company of companiesShortArray) {
+                if (company.toLowerCase().includes(name.toLowerCase())) {
+                    chillFilteredCompanies.push(company);
+                }
+            }
+            res.send({
+                beers: chillFilteredBeers,
+                companies: chillFilteredCompanies
+            });
+        }).catch(err => {
+            console.log('(/filter) Error', err);
+        });
+    });
+});
+
+server.get('/', (req, res) => {});
+
+server.listen(process.env.PORT || 5555, () => {
+    console.log('Which Beer now listening on port', process.env.PORT || 5555);
+});
